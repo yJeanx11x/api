4c8203d7c9ded211a26309abbd1d32b895745318
Jean Lucas <j3anlucas010@gmail.com>
2025-05-13 10:09:17 -0300

    Passando Parâmetros na requisição

--- script.js
+++ script.js
@@ -8,9 +8,20 @@
 //
 
 // Utilizando o fetch com async/await.
+
 async function fetchProduct() {
     const resposta=await fetch("http://localhost:3000/products")
     const data= await resposta.json()
     console.log(data)
 }
 fetchProduct()
+
+// Passando Parâmetros na requisição
+
+async function fetchProductById(id) {
+    const response=await fetch(`http://localhost:3000/products/${id}`)
+    const datas=await response.json()
+console.log(datas)
+}
+
+fetchProductById(2) 
